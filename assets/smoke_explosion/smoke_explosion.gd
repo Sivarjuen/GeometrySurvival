5f60955a162ebf1b5e8e50e375e184d660c0fa3a
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2023-12-30 14:02:52 +0000

    Include .gdshader in export

--- assets/smoke_explosion/smoke_explosion.gd
+++ assets/smoke_explosion/smoke_explosion.gd
@@ -4,6 +4,6 @@ extends Node2D
 func _ready():
 	%Smoke.material.set_shader_parameter("texture_offset", Vector2(randfn(0.0, 1.0), randfn(0.0, 1.0)))
 	%AnimationPlayer.play("explosion")
-	await %AnimationPlayer.animation_finished
-	queue_free()
 
+func _on_animation_player_animation_finished(anim_name):
+	queue_free()
